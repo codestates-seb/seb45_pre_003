edad4934ba68d20ccaaaf847ffab92c3eb244d48
9599562 <9599562@gmail.com>
2023-08-10 22:23:01 +0900

    [BE] Question 엔티티 복구

---
+++ be/src/main/java/com/codestates/stackoverflowclone/question/entity/Question.java
@@ -0,0 +1,32 @@
+package com.codestates.stackoverflowclone.question.entity;
+
+import com.codestates.stackoverflowclone.answer.entity.Answer;
+import com.codestates.stackoverflowclone.audit.Auditable;
+import com.codestates.stackoverflowclone.member.entity.Member;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter@Setter@NoArgsConstructor@AllArgsConstructor@Builder
+@Entity
+public class Question extends Auditable {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String body;
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answers = new ArrayList<>();
+
+    public void addAnswers(Answer answer) {
+        this.answers.add(answer);
+        if (answer.getQuestion() != this) answer.setQuestion(this);
+    }
+
+}
