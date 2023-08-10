a00451a2f7bf9075f567de66322519f0f0cd5348
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 <gmlahr970516@naver.com>
2023-08-10 18:02:28 +0900

    1차pr

--- fe/src/components/Content.jsx
+++ fe/src/components/Content.jsx
@@ -1,4 +1,4 @@
-import { Routes } from "react-router-dom";
+
 import { ContentStyle } from "../style";
 import MainBar from "./MainBar";
 import RightBar from "./RightBar";
@@ -6,10 +6,8 @@ import RightBar from "./RightBar";
 function Content () {
     return (
         <ContentStyle>
-            <Routes>
                 <MainBar></MainBar>
                 <RightBar></RightBar>
-            </Routes>
         </ContentStyle>
     )
 }
--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { HeaderStyle } from "../style";
+import { Link } from 'react-router-dom';
+
 
 function Header () {
     return (
         <HeaderStyle>
-            
+            <Link to="login">로그인페이지</Link>
         </HeaderStyle>
     )
 }
