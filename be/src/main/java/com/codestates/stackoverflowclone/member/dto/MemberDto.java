a76a1b2acd135c65a67b9dacaa2e44bfca9100ca
9599562 <9599562@gmail.com>
2023-08-10 14:18:03 +0900

    [BE] MemberDto.Post 구현

--- be/src/main/java/com/codestates/stackoverflowclone/member/dto/MemberDto.java
+++ be/src/main/java/com/codestates/stackoverflowclone/member/dto/MemberDto.java
@@ -1,4 +1,37 @@
 package com.codestates.stackoverflowclone.member.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class MemberDto {
+    @Getter
+    @Builder
+    public static class Post {
+        //TODO name 조건 생각해보기
+        @NotBlank
+        private String name;
+
+        //TODO email 최대 길이 정하기. 일단은 30자 이하 > member에도 적용하기
+        @Pattern(regexp = "^(?=.{1,30}$)((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$")
+        private String email;
+
+        //TODO password 최대 길이 정하기. 일단은 20자 이하 > member에도 적용하기
+        @Pattern(regexp = "^((?=\\S*?[a-zA-Z])(?=\\S*?[0-9]).{8,20})\\S$")
+        private String password;
+    }
+
+    @Getter
+    @Builder
+    public static class Patch {
+
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+
+    }
 }
--- be/src/main/java/com/codestates/stackoverflowclone/member/mapper/MemberMapper.java
+++ be/src/main/java/com/codestates/stackoverflowclone/member/mapper/MemberMapper.java
@@ -1,4 +1,7 @@
 package com.codestates.stackoverflowclone.member.mapper;
 
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
 }
