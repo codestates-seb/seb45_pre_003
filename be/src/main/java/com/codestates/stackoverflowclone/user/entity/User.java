c9be19997500b762946c415155caa8e9dd77796f
9599562 <9599562@gmail.com>
2023-08-10 11:18:31 +0900

    [BE] users 개발중

--- be/src/main/java/com/codestates/stackoverflowclone/user/entity/User.java
+++ be/src/main/java/com/codestates/stackoverflowclone/user/entity/User.java
@@ -1,4 +1,28 @@
 package com.codestates.stackoverflowclone.user.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
 }
