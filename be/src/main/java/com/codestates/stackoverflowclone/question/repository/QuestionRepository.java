1775bd5c39b905015868fc74e27ca3bba0da14aa
JeonHaesung <eternalasset92@gmail.com>
2023-08-10 13:57:45 +0900

    repository, entity 작성

--- be/src/main/java/com/codestates/stackoverflowclone/question/repository/QuestionRepository.java
+++ be/src/main/java/com/codestates/stackoverflowclone/question/repository/QuestionRepository.java
@@ -1,4 +1,8 @@
 package com.codestates.stackoverflowclone.question.repository;
 
-public interface QuestionRepository {
+import com.codestates.stackoverflowclone.question.entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
 }
