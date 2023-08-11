030ada74c1e8c019a5d32584d09dd3bee0d90505
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 <gmlahr970516@naver.com>
2023-08-11 12:02:48 +0900

    4차pr

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
-import Container from './components/Container'
+// import Container from './components/Container'
 import Footer from './components/Footer'
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 import MyPage from './pages/MyPage';
-import QuestionPage from './pages/QuestionPage';
 import SignupPage from './pages/SignupPage';
 
 function App() {
@@ -13,12 +12,11 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/mypage" element={<MyPage/>}/>
-          <Route path="/question" element={<QuestionPage/>}/>
-          <Route path="/signup" element={<SignupPage/>}/>
-        </Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/mypage" element={<MyPage/>}/>
+        <Route path="/signup" element={<SignupPage/>}/>
+      </Routes>
       <Footer/>
     </BrowserRouter>
   );
