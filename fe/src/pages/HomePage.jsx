ffba03b3a6636194e09251e1486c044875c6e090
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 <gmlahr970516@naver.com>
2023-08-11 10:53:52 +0900

    2차 pr

--- fe/src/pages/HomePage.jsx
+++ fe/src/pages/HomePage.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import LeftBar from "../components/LeftBar";
+import { HomePageContentStyle, HomePageMainBarStyle, HomePageRightBarStyle, TopBox, Title, AskQuestionBtn, SecondBox, QuestionsNum, FilterBox, FirstFilter, SecondFilter, ThirdFilter, ForthFilter, LastFilter, Ul, Li, LiStatusBox, StatsItem, StatsItemNumber, StatsItemUnit, LiContentBox, LiTitle, LiTag, LiTagAuthorBox, MetaData } from "../components/homepage/HomePage.style";
+import { ContainerStyle } from "../style";
 
-export default function HomePage() {
-  return (
-    <div>
-        HomePage
-    </div>
-  )
+const DummyData = [
+    {
+        title:'this is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test titlethis is test title',
+        tag:[],
+        author:'stefan',
+        imageUrl : '',
+        createdAt:'modified 59 secs ago',
+        modified:true,
+        votes:0,
+        answers:0,
+        views:2,
+    },
+    {
+        title:'this is second test title',
+        tag:['javascript','c++','c#'],
+        author:'starball',
+        imageUrl : '',
+        createdAt:'asked 9 secs ago',
+        votes:5,
+        answers:2,
+        views:3,
+    },
+    {
+        title:'this is test title',
+        tag:['python','image','java'],
+        author:'stefan',
+        imageUrl : '',
+        createdAt:'asked 59 min ago',
+        votes:0,
+        answers:0,
+        views:2,
+    },
+    {
+        title:'this is second test title',
+        tag:['javascript','c++','c#'],
+        author:'starball',
+        imageUrl : '',
+        createdAt:'',
+        votes:5,
+        answers:2,
+        views:3,
+    },
+    {
+        title:'this is test title',
+        tag:['python','image','java'],
+        author:'stefan',
+        imageUrl : '',
+        createdAt:'',
+        votes:0,
+        answers:0,
+        views:2,
+    },
+    {
+        title:'this is second test title',
+        tag:['javascript','c++','c#'],
+        author:'starball',
+        imageUrl : '',
+        createdAt:'',
+        votes:5,
+        answers:2,
+        views:3,
+    },
+    {
+        title:'this is test title',
+        tag:['python','image','java'],
+        author:'stefan',
+        imageUrl : '',
+        createdAt:'',
+        votes:0,
+        answers:0,
+        views:2,
+    },
+    {
+        title:'this is second test title',
+        tag:['javascript','c++','c#'],
+        author:'starball',
+        imageUrl : '',
+        createdAt:'',
+        votes:5,
+        answers:2,
+        views:3,
+    },
+]
+
+
+function HomePage () {
+    
+    return (
+        <ContainerStyle>
+            <LeftBar/>
+            <HomePageContentStyle>
+                <HomePageMainBarStyle>
+                    <TopBox>
+                        <Title>Top Questions</Title>
+                        <AskQuestionBtn>Ask Question</AskQuestionBtn>
+                    </TopBox>
+                    <SecondBox>
+                        <QuestionsNum></QuestionsNum>
+                        <FilterBox>
+                            <FirstFilter>interesting</FirstFilter>
+                            <SecondFilter>Bountied</SecondFilter>
+                            <ThirdFilter>Hot</ThirdFilter>
+                            <ForthFilter>Week</ForthFilter>
+                            <LastFilter>Month</LastFilter>
+                        </FilterBox>
+                    </SecondBox>
+                    <Ul>
+                        {DummyData.map((el,idx)=>{
+                            return (
+                                <Li key={idx}>
+                                    <LiStatusBox>
+                                        <StatsItem>
+                                            <StatsItemNumber>{el.votes}</StatsItemNumber>
+                                            <StatsItemUnit>votes</StatsItemUnit>
+                                        </StatsItem>
+                                        <StatsItem>
+                                            <StatsItemNumber>{el.answers}</StatsItemNumber>
+                                            <StatsItemUnit>answers</StatsItemUnit>
+                                        </StatsItem>
+                                        <StatsItem>
+                                            <StatsItemNumber>{el.views}</StatsItemNumber>
+                                            <StatsItemUnit>views</StatsItemUnit>
+                                        </StatsItem>
+                                    </LiStatusBox>
+                                    <LiContentBox>
+                                        <LiTitle>{el.title}</LiTitle>
+                                        <LiTagAuthorBox>
+                                            {el.tag.map((_,idx)=>{
+                                                return (
+                                                    <LiTag key={idx}>{el.tag[idx]}</LiTag>
+                                                )
+                                            })}
+                                            <MetaData>
+                                                <img src="" alt="img"/>
+                                                <a href="/" >{el.author}</a>
+                                                <a href="/" style={{color:el.modified ? 'gray' : 'black'}} >{el.createdAt}</a>
+                                            </MetaData>
+                                        </LiTagAuthorBox>
+                                    </LiContentBox>
+                                </Li>
+                            )
+                        })}
+                    </Ul>
+                </HomePageMainBarStyle>
+                <HomePageRightBarStyle>
+                </HomePageRightBarStyle>
+            </HomePageContentStyle>
+        </ContainerStyle>
+    )
 }
+
+export default HomePage;
--- fe/src/pages/MyPage.jsx
+++ fe/src/pages/MyPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { React, useState } from 'react'
+import Header from '../components/Header'; 
+import LeftBar from '../components/LeftBar';
+import MypageContent from '../components/mypageComponents/MyPageContent';
+import { MyPageContainerStyle } from '../components/mypageComponents/MyPage.styled';
+import { MypageContentStyle } from  '../components/mypageComponents/MyPage.styled';
+import Footer from '../components/Footer';
+
 
 export default function MyPage() {
+
+   const [selectedContent, setSelectedContent] = useState('Activity');
+
   return (
-    <div>
-        MyPage
-    </div>
-  )
+    <>
+    <Header />
+    <MyPageContainerStyle>
+        <LeftBar/>
+        <MypageContentStyle>
+          <MypageContent 
+          selectedContent={selectedContent}
+          setSelectedContent={setSelectedContent}
+          />
+        </MypageContentStyle>
+    </MyPageContainerStyle>
+    <Footer />
+    </>  
+    )
 }
